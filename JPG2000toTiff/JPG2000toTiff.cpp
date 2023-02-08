918d71e4a1a252765d60fac531bcf208b2712957
José Ángel <joseangelmt@outlook.com>
2023-02-08 12:01:01 +0100

    Ahora se pueden pasar imágenes en cuadrícula

--- JPG2000toTiff/JPG2000toTiff.cpp
+++ JPG2000toTiff/JPG2000toTiff.cpp
@@ -13,8 +13,95 @@
 
 #define TAMANO_TILE 1024
 
+unsigned FilaImagenTotalANumeroImagenVertical(unsigned filaImagenTotal, NCSFileViewFileInfoEx** fileInfo, unsigned columnasImagenes, unsigned filasImagenes)
+{
+	auto filaImagen = 0u;
+	auto filaLectura = 0u;
+	auto acumulado = 0u;
+	for (auto i = 0u; i < filasImagenes; i++)
+	{
+		auto const altoEstaImagen = fileInfo[static_cast<size_t>(i * columnasImagenes)]->nSizeY;
+		if (filaImagenTotal < acumulado + altoEstaImagen)
+		{
+			return i;
+		}
+
+		acumulado += altoEstaImagen;
+	}
+
+	return {};
+}
+
+unsigned FilaImagenTotalAFilaLectura(unsigned filaImagenTotal, NCSFileViewFileInfoEx** fileInfo, unsigned columnasImagenes, unsigned filasImagenes)
+{
+	auto filaImagen = 0u;
+	auto filaLectura = 0u;
+	auto acumulado = 0u;
+	for (auto i = 0u; i < filasImagenes; i++)
+	{
+		auto const altoEstaImagen = fileInfo[static_cast<size_t>(i * columnasImagenes)]->nSizeY;
+		if (filaImagenTotal < acumulado + altoEstaImagen)
+		{
+			return filaImagenTotal - acumulado;
+		}
+
+		acumulado += altoEstaImagen;
+	}
+
+	return {};
+}
+
+unsigned ColumnaImagenTotalANumeroImagenHorizontal(unsigned columnaImagenTotal, NCSFileViewFileInfoEx** fileInfo, unsigned columnasImagenes, unsigned filasImagenes)
+{
+	auto filaImagen = 0u;
+	auto filaLectura = 0u;
+	auto acumulado = 0u;
+	for (auto i = 0u; i < columnasImagenes; i++)
+	{
+		auto const anchoEstaImagen = fileInfo[static_cast<size_t>(i * columnasImagenes)]->nSizeX;
+		if (columnaImagenTotal < acumulado + anchoEstaImagen)
+		{
+			return i;
+		}
+
+		acumulado += anchoEstaImagen;
+	}
+
+	return {};
+}
+
+unsigned ColumnaImagenTotalAColumnaLectura(unsigned columnaImagenTotal, NCSFileViewFileInfoEx** fileInfo, unsigned columnasImagenes, unsigned filasImagenes)
+{
+	auto filaImagen = 0u;
+	auto filaLectura = 0u;
+	auto acumulado = 0u;
+	for (auto i = 0u; i < columnasImagenes; i++)
+	{
+		auto const anchoEstaImagen = fileInfo[static_cast<size_t>(i * columnasImagenes)]->nSizeX;
+		if (columnaImagenTotal < acumulado + anchoEstaImagen)
+		{
+			return columnaImagenTotal - acumulado;
+		}
+
+		acumulado += anchoEstaImagen;
+	}
+
+	return {};
+}
+
+template<typename T>
+auto IndiceImagenHorizontalAColumnaImagenTotal(T imagenHorizontal, NCSFileViewFileInfoEx** fileInfo)
+{
+	auto acumulado = 0;
+	for (T i = {}; i < imagenHorizontal; ++i)
+		acumulado += fileInfo[i]->nSizeX;
+
+	return acumulado;
+}
+
+
 template<typename TipoTiff, NCSCellType TipoEcw>
-int Trabaja(TIFF* tiff, NCSFileView** fileView, NCSFileViewFileInfoEx** fileInfo, size_t imagenes)
+int Trabaja(TIFF* tiff, NCSFileView** fileView, NCSFileViewFileInfoEx** fileInfo, size_t imagenes, int columnasImagenes, int filasImagenes)
 {
 	auto const samplesPerPixel = fileInfo[0]->nBands;
 
@@ -32,85 +119,118 @@ int Trabaja(TIFF* tiff, NCSFileView** fileView, NCSFileViewFileInfoEx** fileInfo
 		return 2;
 	}
 
+	UINT32 anchoTotal{};
 	UINT32 altoTotal{};
-	for(auto i=0u; i<imagenes; i++)
-		altoTotal += fileInfo[i]->nSizeY;
+	unsigned indiceImagen = 0;
+	for(auto filaImagen=0; filaImagen<filasImagenes; filaImagen++)
+	{
+		altoTotal += fileInfo[indiceImagen]->nSizeY;
 
-	auto const tilesHorizontal = (fileInfo[0]->nSizeX + TAMANO_TILE - 1) / TAMANO_TILE;
+		for (auto columnaImagen = 0; columnaImagen < columnasImagenes; columnaImagen++, indiceImagen++)
+		{
+			if (filaImagen == 0) 
+			{
+				anchoTotal += fileInfo[indiceImagen]->nSizeX;
+			}
+		}
+	}
 
-	TIFFSetField(tiff, TIFFTAG_IMAGEWIDTH, fileInfo[0]->nSizeX);
+	auto const tilesHorizontal = (anchoTotal + TAMANO_TILE - 1) / TAMANO_TILE;
+
+	TIFFSetField(tiff, TIFFTAG_IMAGEWIDTH, anchoTotal);
 	TIFFSetField(tiff, TIFFTAG_IMAGELENGTH, altoTotal);
 	TIFFSetField(tiff, TIFFTAG_BITSPERSAMPLE, static_cast<uint16_t>(8) * sizeof(TipoTiff));
 	TIFFSetField(tiff, TIFFTAG_COMPRESSION, COMPRESSION_NONE);
 	TIFFSetField(tiff, TIFFTAG_SAMPLESPERPIXEL, samplesPerPixel);
 	TIFFSetField(tiff, TIFFTAG_PHOTOMETRIC, photommetric);
-	//TIFFSetField(tiff, TIFFTAG_SOFTWARE, L"Digi3D.NET");
+	TIFFSetField(tiff, TIFFTAG_SOFTWARE, L"Digi3D.NET");
 	TIFFSetField(tiff, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
 	TIFFSetField(tiff, TIFFTAG_TILEWIDTH, TAMANO_TILE);
 	TIFFSetField(tiff, TIFFTAG_TILELENGTH, TAMANO_TILE);
 
-	auto** tilesDestino = new TipoTiff * [tilesHorizontal];
-	for (auto i = 0u; i < tilesHorizontal; i++) {
-		tilesDestino[i] = new TipoTiff[TAMANO_TILE * TAMANO_TILE * samplesPerPixel];
-	}
+	auto destinoTemp = std::unique_ptr<TipoTiff[]>{ new TipoTiff[tilesHorizontal * TAMANO_TILE * TAMANO_TILE * samplesPerPixel] };
+	auto tileDestino = std::unique_ptr<TipoTiff[]>{ new TipoTiff[TAMANO_TILE * TAMANO_TILE * samplesPerPixel] };
 
 	UINT listaBandas[]{ 0,1,2,3 };
 
 	auto* p_p_output_line = new TipoTiff * [samplesPerPixel];
 	for (auto i = 0; i < samplesPerPixel; i++)
-		p_p_output_line[i] = new TipoTiff[fileInfo[0]->nSizeX];
+		p_p_output_line[i] = new TipoTiff[anchoTotal];
 
 	auto oldPorciento = -1;
-	auto filaTotal = 0u;
 	int filaEnTile = 0;
-	for (auto i = 0u; i < imagenes; i++) {
-		if (NCS_SUCCESS != NCScbmSetFileView(
-			fileView[i],
-			fileInfo[i]->nBands,
-			listaBandas,
-			0,
-			0,
-			fileInfo[i]->nSizeX - 1,
-			fileInfo[i]->nSizeY - 1,
-			fileInfo[i]->nSizeX,
-			fileInfo[i]->nSizeY))
-			return 1;
 
-		for (auto fila = 0u; fila < fileInfo[i]->nSizeY; fila++, filaTotal++) {
-			if (auto const porciento = static_cast < int>(100 * filaTotal / altoTotal); porciento != oldPorciento) {
-				oldPorciento = porciento;
-				std::cout << porciento << "%" << std::endl;
-			}
-			if (filaTotal + 1 == altoTotal)
-				break;
+	auto filaDentroDeTeselasDeEscritura = 0;
+	for(auto filaTotal = 0u; filaTotal < altoTotal;)
+	{
+		// Averiguamos la fila de imágenes que va a proporcionar información para esta fila
+		auto const imagenVertical = FilaImagenTotalANumeroImagenVertical(filaTotal, fileInfo, columnasImagenes, filasImagenes);
+		auto filaLectura = FilaImagenTotalAFilaLectura(filaTotal, fileInfo, columnasImagenes, filasImagenes);
 
-			if(NCS_READ_OK != NCScbmReadViewLineBILEx(fileView[i], TipoEcw, reinterpret_cast<void**>(p_p_output_line)))
+		for(auto imagenHorizontal = 0; imagenHorizontal < columnasImagenes; imagenHorizontal++ )
+		{
+			auto const indiceImagenLectura = imagenVertical * columnasImagenes + imagenHorizontal;
+
+			if (NCS_SUCCESS != NCScbmSetFileView(
+				fileView[indiceImagenLectura],
+				fileInfo[indiceImagenLectura]->nBands,
+				listaBandas,
+				0,
+				filaLectura,
+				fileInfo[indiceImagenLectura]->nSizeX - 1,
+				filaLectura + min(fileInfo[indiceImagenLectura]->nSizeY - filaLectura, TAMANO_TILE) - 1,
+				fileInfo[indiceImagenLectura]->nSizeX,
+				min(fileInfo[indiceImagenLectura]->nSizeY - filaLectura, TAMANO_TILE)))
+			{
 				return 1;
+			}
 
-			for (auto tileHorizontal = 0u; tileHorizontal < tilesHorizontal; tileHorizontal++) {
-				auto* destino = tilesDestino[tileHorizontal] + filaEnTile * TAMANO_TILE * samplesPerPixel;
-				auto const numeroColumnas = (tileHorizontal * TAMANO_TILE + TAMANO_TILE > fileInfo[0]->nSizeX ? fileInfo[0]->nSizeX - tileHorizontal * TAMANO_TILE : TAMANO_TILE);
+			auto const columnaDestinoInicialEstaImagenLectura = IndiceImagenHorizontalAColumnaImagenTotal(imagenHorizontal, fileInfo);
 
-				for (auto columna = 0u; columna < numeroColumnas; columna++) {
+			auto copiaFilaDentroDeTeselasDeEscritura = filaDentroDeTeselasDeEscritura;
+			auto copiaFilaLecturaDeImagenesOriginales = filaLectura;
+			// Recorremos las filas restantes de la tesela a rellenar
+			for(; copiaFilaDentroDeTeselasDeEscritura < TAMANO_TILE && copiaFilaLecturaDeImagenesOriginales < fileInfo[indiceImagenLectura]->nSizeY; copiaFilaDentroDeTeselasDeEscritura++, copiaFilaLecturaDeImagenesOriginales++)
+			{
+				if (NCS_READ_OK != NCScbmReadViewLineBILEx(fileView[indiceImagenLectura], TipoEcw, reinterpret_cast<void**>(p_p_output_line)))
+					return 1;
+
+				auto* destino = destinoTemp + (copiaFilaDentroDeTeselasDeEscritura * tilesHorizontal * TAMANO_TILE * samplesPerPixel) + (columnaDestinoInicialEstaImagenLectura * samplesPerPixel);
+				for (auto columna = 0u; columna < fileInfo[indiceImagenLectura]->nSizeX; columna++) {
 					for (auto banda = 0; banda < samplesPerPixel; banda++)
-						*destino++ = p_p_output_line[banda][tileHorizontal * TAMANO_TILE + columna];
+						*destino++ = p_p_output_line[banda][columna];
 				}
 			}
 
-			filaEnTile++;
-			if (filaEnTile == TAMANO_TILE) {
-				// Se ha rellenado la tesela. La almacenamos.
-				filaEnTile = 0;
+			if(imagenHorizontal == columnasImagenes - 1)
+			{
+				filaDentroDeTeselasDeEscritura = copiaFilaDentroDeTeselasDeEscritura;
 
-				for (auto tileHorizontal = 0u; tileHorizontal < tilesHorizontal; tileHorizontal++) {
-					TIFFWriteTile(tiff, static_cast<void*>(tilesDestino[tileHorizontal]), tileHorizontal * TAMANO_TILE, filaTotal, 0, 0);
+				if(filaDentroDeTeselasDeEscritura == TAMANO_TILE || copiaFilaDentroDeTeselasDeEscritura >= altoTotal)
+				{
+					// Hemos rellenado el buffer temporal, de manera que tenemos que almacenar
+					for (auto tileHorizontal = 0u; tileHorizontal < tilesHorizontal; tileHorizontal++) {
+						for (auto i = 0; i < TAMANO_TILE; i++)
+							memcpy(tileDestino + i * TAMANO_TILE * samplesPerPixel, 
+								destinoTemp + i * tilesHorizontal * TAMANO_TILE * samplesPerPixel + tileHorizontal * TAMANO_TILE * samplesPerPixel,
+								TAMANO_TILE * samplesPerPixel);
+
+						TIFFWriteTile(tiff, static_cast<void*>(tileDestino), tileHorizontal * TAMANO_TILE, filaTotal, 0, 0);
+					}
+					filaDentroDeTeselasDeEscritura = 0;
 				}
+
+				filaLectura = copiaFilaLecturaDeImagenesOriginales;
+				filaTotal += copiaFilaDentroDeTeselasDeEscritura;
+
+				if(auto const porciento = filaTotal * 100 / altoTotal; porciento != oldPorciento )
+				{
+					std::cout << porciento << "%" << std::endl;
+					oldPorciento = porciento;
+				}
+
 			}
 		}
-	}
-
-	for (auto tileHorizontal = 0u; tileHorizontal < tilesHorizontal; tileHorizontal++) {
-		TIFFWriteTile(tiff, static_cast<void*>(tilesDestino[tileHorizontal]), tileHorizontal * TAMANO_TILE, filaTotal, 0, 0);
 	}
 
 	TIFFClose(tiff);
@@ -121,25 +241,37 @@ int wmain(int args, wchar_t* argv[])
 {
 	std::cout << "JPG2000toTiff" << std::endl;
 
-	if( args < 3 )
+	if( args < 5 )
 	{
 		std::cerr << "Error: Not enough parameters have been specified." << std::endl;
 		std::cerr << "The format is:" << std::endl;
-		std::cerr << "JPG2000toTIFF [tiff file to be created] [jp2 file number 1]...[jp2 file number n]" << std::endl;
+		std::cerr << "JPG2000toTIFF [tiff file to be created] [num of cols] [num of rows] [jp2 file number 1]...[jp2 file number n] " << std::endl;
 		std::cerr << "If more than one .jp2 file is specified, a single TIFF will be created in which the different .jp2 files will be concatenated from top to bottom." << std::endl;
 		return 1;
 	}
 
 	std::wstring archivoCrear{ argv[1] };
-	auto const archivosJp2{ args - 2 };
 
-	std::unique_ptr< NCSFileView* []> fileView{ new NCSFileView * [archivosJp2] };
-	std::unique_ptr<NCSFileViewFileInfoEx* []> fileInfo{ new NCSFileViewFileInfoEx * [archivosJp2] };
+	auto const imagenesHorizontal = _wtoi(argv[2]);
+	auto const imagenesVertical = _wtoi(argv[3]);
 
-	for(auto i=0; i< archivosJp2; i++)
+	if( imagenesHorizontal * imagenesVertical != args - 4)
 	{
-		if (NCScbmOpenFileView(argv[2 + i], &fileView[i], nullptr)  != NCS_SUCCESS)
+		std::wcerr << L"No has pasado por parámetros exactamente " << imagenesHorizontal * imagenesVertical << " imágenes" << std::endl;
+		return 1;
+	}
+
+	auto const numeroImagenes = imagenesHorizontal * imagenesVertical;
+
+	std::unique_ptr< NCSFileView* []> fileView{ new NCSFileView * [numeroImagenes] };
+	std::unique_ptr<NCSFileViewFileInfoEx* []> fileInfo{ new NCSFileViewFileInfoEx * [numeroImagenes] };
+
+	for(auto i=0; i< numeroImagenes; i++)
+	{
+		if (NCScbmOpenFileView(argv[4 + i], &fileView[i], nullptr) != NCS_SUCCESS) {
+			std::wcerr << L"Error al abrir el archivo: " << argv[4 + i] << std::endl;
 			return 1;
+		}
 
 		NCScbmGetViewFileInfoEx(fileView[i], &fileInfo[i]);
 	}
@@ -160,9 +292,9 @@ int wmain(int args, wchar_t* argv[])
 	switch (fileInfo[0]->eCellType)
 	{
 	case NCSCT_UINT8:
-		return Trabaja<BYTE, NCSCT_UINT8>(tiff, fileView.get(), fileInfo.get(), archivosJp2);
+		return Trabaja<BYTE, NCSCT_UINT8>(tiff, fileView.get(), fileInfo.get(), numeroImagenes, imagenesHorizontal, imagenesVertical);
 	case NCSCT_UINT16:
-		return Trabaja<USHORT, NCSCT_UINT16>(tiff, fileView.get(), fileInfo.get(), archivosJp2);
+		return Trabaja<USHORT, NCSCT_UINT16>(tiff, fileView.get(), fileInfo.get(), numeroImagenes, imagenesHorizontal, imagenesVertical);
 	default:
 		return 2;
 	}
